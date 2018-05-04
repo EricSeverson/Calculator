3d1cec1cd7800bbf56bb28382a4914f7aa0fce5d
Eric James Severson <severson_eric@yahoo.com>
2018-05-04 01:35:40 -0700

    Functional Calculator now. No operator chaining just yet.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Calculator
+Basic calculator.
+Most functionality implemented. needs polishing. Will come back to this at a later date.
--- script.js
+++ script.js
@@ -1,46 +1,85 @@
-function add(x, y) {
-  return x + y;
-}
+class Expression {
 
-function subtract(x, y) {
-  return x - y;
-}
-
-function multiply(x, y) {
-  return x * y;
-}
-
-function divide(x, y) {
-  return x / y;
-}
-
-function operate(operator, x, y) {
-  if (operator === '+') {
-    return add(x, y);
-  } else if (operator === '-') {
-    return subtract(x, y);
-  } else if (operator === '*') {
-    return multiply(x, y);
-  } else if (operator === '/') {
-    return divide(x, y);
+  constructor(leftOperand, operator, rightOperand) {
+    this.leftOperand = leftOperand;
+    this.operator = operator;
+    this.rightOperand = rightOperand;
   }
 }
 
-function populateNumber(e) {
-  let textBox = document.querySelector('.screen input');
-  let pressedButton = e.target.textContent;
-  if (pressedButton === 'C') {
-    clearScreen();
-    return;
+class Calculator {
+
+  constructor() {
+    this.textBox = document.querySelector('.screen input');
+    this.expression = new Expression();
   }
-    if (textBox.value === '0') textBox.value = pressedButton;
-  else textBox.value = textBox.value + pressedButton;
-}
-function clearScreen() {
-  document.querySelector('.screen input').value = '0';
+
+  add(x, y) {
+    return x + y;
+  }
+
+  subtract(x, y) {
+    return x - y;
+  }
+
+  multiply(x, y) {
+    return x * y;
+  }
+
+  divide(x, y) {
+    return x / y;
+  }
+
+  operate(operator, x, y) {
+    if (operator === '+') {
+      return this.add(x, y);
+    } else if (operator === '-') {
+      return this.subtract(x, y);
+    } else if (operator === '*') {
+      return this.multiply(x, y);
+    } else if (operator === '/') {
+      return this.divide(x, y);
+    }
+  }  
+
+  populateNumber(e) {
+    let pressedButton = e.target.textContent;
+    if (pressedButton === 'C') {
+      this.clearScreen();
+      return;
+    }
+    if (this.textBox.value === '0') this.textBox.value = pressedButton;
+    else if (!parseInt(this.textBox.value)) this.textBox.value = pressedButton;
+    else this.textBox.value = this.textBox.value + pressedButton;
+  }
+
+  populateOperator(e) {
+    if (!parseInt(this.textBox.value)) return;
+    if (e.target.textContent === '=' && parseInt(this.textBox.value)) {
+      if(this.expression.leftOperand && this.expression.operator) {
+        this.expression.rightOperand = this.textBox.value;
+        this.textBox.value = this.operate(this.expression.operator, parseInt(this.expression.leftOperand), parseInt(this.expression.rightOperand));
+        this.expression.leftOperand = this.textBox.value;
+      }
+      return;
+    }
+    if (!this.expression.leftOperand) {
+      this.expression.leftOperand = this.textBox.value;
+    }
+    this.textBox.value = e.target.textContent;
+    this.expression.operator = e.target.textContent;
+  }
+
+  clearScreen() {
+    this.textBox.value = '0';
+    delete this.expression.leftOperand;
+    delete this.expression.rightOperand;
+    delete this.expression.operator;
+  }
 }
 
-console.log(document.querySelectorAll('.numbers button'));
-document.querySelectorAll('.numbers button').forEach(button => addEventListener('click', populateNumber));
+let calculator = new Calculator();
+window.onload = calculator.clearScreen();
 
-window.onload = clearScreen();
+document.querySelector('.numbers').addEventListener('click', calculator.populateNumber.bind(calculator));
+document.querySelector('.operators').addEventListener('click', calculator.populateOperator.bind(calculator));
